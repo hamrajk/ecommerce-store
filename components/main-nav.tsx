1751da8c6a34fa051e288ae504a6b3b4ba0b3e53
hamrajk <hamraj_khangura@hotmail.com>
2023-10-08 21:29:11 -0400

    created billboard and overview pages on admin dashboard

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -14,13 +14,27 @@ export function MainNav({
 
   const routes = [
     {
+      href: `/${params.storeId}/`,
+      label: "Overview",
+      active: pathname === `/${params.storeId}`,
+    },
+    {
+      href: `/${params.storeId}/billboards`,
+      label: "Billboards",
+      active: pathname === `/${params.storeId}/billboard`,
+    },
+    {
       href: `/${params.storeId}/settings`,
       label: "Settings",
-      active: pathname === `${params.storeId}/settings`,
+      active: pathname === `/${params.storeId}/settings`,
     },
   ];
+
   return (
-    <nav className={cn("flex items-center space-x-4 lg:space-x-6", className)}>
+    <nav
+      className={cn("flex items-center space-x-4 lg:space-x-6", className)}
+      {...props}
+    >
       {routes.map((route) => (
         <Link
           key={route.href}
