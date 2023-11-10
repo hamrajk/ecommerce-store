8edcdd220b9b1ea8e52e83f74301166a63ff15d4
hamrajk <hamraj_khangura@hotmail.com>
2023-11-09 22:58:13 -0500

    fixed typo, sizeId instead of sizeID

--- app/api/[storeId]/products/route.ts
+++ app/api/[storeId]/products/route.ts
@@ -92,7 +92,7 @@ export async function GET(
     const { searchParams } = new URL(req.url);
     const categoryId = searchParams.get("categoryId") || undefined;
     const colorId = searchParams.get("colorId") || undefined;
-    const sizeId = searchParams.get("sizeID") || undefined;
+    const sizeId = searchParams.get("sizeId") || undefined;
     const isFeatured = searchParams.get("isFeatured");
 
     if (!params.storeId) {
